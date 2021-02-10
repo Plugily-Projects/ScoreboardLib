aa7316b75abc0a30d9177f55b8527c4a0d0649c3
Tigerpanzer_02 <legionofunicorns0202@gmail.com>
2021-02-10 20:57:27 +0100

    Updated versions

--- src/main/java/me/tigerhix/lib/scoreboard/util/ServerVersion.java
+++ src/main/java/me/tigerhix/lib/scoreboard/util/ServerVersion.java
@@ -4,102 +4,115 @@ import org.bukkit.Bukkit;
 
 public class ServerVersion {
 
-	public Version getVersion() {
-		return Version.getCurrent();
-	}
+  public Version getVersion() {
+    return Version.getCurrent();
+  }
 
-	public enum Version {
-		v1_11_R1,
-		v1_12_R1,
-		v1_13_R1,
-		v1_13_R2,
-		v1_14_R1,
-		v1_14_R2,
-		v1_15_R1,
-		v1_15_R2,
-		v1_16_R1,
-		v1_16_R2,
-		v1_16_R3,
-		v1_17_R1,
-		v1_17_R2;
+  public enum Version {
+    v1_7_R1,
+    v1_8_R1,
+    v1_8_R2,
+    v1_8_R3,
+    v1_9_R1,
+    v1_9_R2,
+    v1_10_R1,
+    v1_11_R1,
+    v1_12_R1,
+    v1_13_R1,
+    v1_13_R2,
+    v1_14_R1,
+    v1_14_R2,
+    v1_15_R1,
+    v1_15_R2,
+    v1_16_R1,
+    v1_16_R2,
+    v1_16_R3,
+    v1_17_R1,
+    v1_17_R2,
+    v1_18_R1,
+    v1_18_R2;
 
-		private Integer value;
-		private String shortVersion;
-		private String packageVersion;
-		private static Version current = null;
+    private final Integer value;
+    private final String shortVersion;
+    private final String packageVersion;
+    private static Version current;
 
-		Version() {
-			value = Integer.valueOf(name().replaceAll("[^\\d.]", ""));
-			shortVersion = name().substring(0, name().length() - 3);
-			packageVersion = Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];
-		}
+    Version() {
+      value = Integer.valueOf(name().replaceAll("[^\\d.]", ""));
+      shortVersion = name().substring(0, name().length() - 3);
+      packageVersion = Bukkit.getServer().getClass().getPackage().getName().replace('.', ',').split(",")[3];
+    }
 
-		public Integer getValue() {
-			return value;
-		}
+    public Integer getValue() {
+      return value;
+    }
 
-		public String getShortVersion() {
-			return shortVersion;
-		}
+    public String getShortVersion() {
+      return shortVersion;
+    }
 
-		public String getPackageVersion() {
-			return packageVersion;
-		}
+    public String getPackageVersion() {
+      return packageVersion;
+    }
 
-		public static Version getCurrent() {
-			if (current != null)
-				return current;
+    public static Version getCurrent() {
+      if(current != null)
+        return current;
 
-			String[] v = Bukkit.getServer().getClass().getPackage().getName().split("\\.");
-			String vv = v[v.length - 1];
-			for (Version one : values()) {
-				if (one.name().equalsIgnoreCase(vv)) {
-					current = one;
-					break;
-				}
-			}
+      String[] v = Bukkit.getServer().getClass().getPackage().getName().split("\\.");
+      String vv = v[v.length - 1];
+      for(Version one : values()) {
+        if(one.name().equalsIgnoreCase(vv)) {
+          current = one;
+          break;
+        }
+      }
 
-			return current;
-		}
+      if (current == null) { // If we forgot to add new version to enum
+        current = Version.v1_11_R1;
+      }
 
-		public boolean isLower(Version version) {
-			return getValue() < version.getValue();
-		}
+      return current;
+    }
 
-		public boolean isHigher(Version version) {
-			return getValue() > version.getValue();
-		}
+    public boolean isLower(Version version) {
+      return getValue() < version.getValue();
+    }
 
-		public boolean isEqual(Version version) {
-			return getValue().equals(version.getValue());
-		}
+    public boolean isHigher(Version version) {
+      return getValue() > version.getValue();
+    }
 
-		public boolean isEqualOrLower(Version version) {
-			return getValue() <= version.getValue();
-		}
+    public boolean isEqual(Version version) {
+      return getValue().equals(version.getValue());
+    }
 
-		public boolean isEqualOrHigher(Version version) {
-			return getValue() >= version.getValue();
-		}
+    public boolean isEqualOrLower(Version version) {
+      return getValue() <= version.getValue();
+    }
 
-		public static boolean isCurrentEqualOrHigher(Version v) {
-			return getCurrent().getValue() >= v.getValue();
-		}
+    public boolean isEqualOrHigher(Version version) {
+      return getValue() >= version.getValue();
+    }
 
-		public static boolean isCurrentHigher(Version v) {
-			return getCurrent().getValue() > v.getValue();
-		}
+    public static boolean isCurrentEqualOrHigher(Version v) {
+      return getCurrent().getValue() >= v.getValue();
+    }
 
-		public static boolean isCurrentLower(Version v) {
-			return getCurrent().getValue() < v.getValue();
-		}
+    public static boolean isCurrentHigher(Version v) {
+      return getCurrent().getValue() > v.getValue();
+    }
 
-		public static boolean isCurrentEqualOrLower(Version v) {
-			return getCurrent().getValue() <= v.getValue();
-		}
+    public static boolean isCurrentLower(Version v) {
+      return getCurrent().getValue() < v.getValue();
+    }
 
-		public static boolean isCurrentEqual(Version v) {
-			return getCurrent().getValue().equals(v.getValue());
-		}
-	}
+    public static boolean isCurrentEqualOrLower(Version v) {
+      return getCurrent().getValue() <= v.getValue();
+    }
+
+    public static boolean isCurrentEqual(Version v) {
+      return getCurrent().getValue().equals(v.getValue());
+    }
+  }
 }
