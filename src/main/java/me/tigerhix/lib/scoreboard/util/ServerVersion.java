a21d3e98367a33cf85b1c4189ba9a4a2e26f7e3f
Tigerpanzer_02 <legionofunicorns0202@gmail.com>
2022-06-17 20:32:29 +0200

    Added 1.19 ServerVersion

--- src/main/java/me/tigerhix/lib/scoreboard/util/ServerVersion.java
+++ src/main/java/me/tigerhix/lib/scoreboard/util/ServerVersion.java
@@ -31,7 +31,9 @@ public class ServerVersion {
     v1_17_R1,
     v1_17_R2,
     v1_18_R1,
-    v1_18_R2;
+    v1_18_R2,
+    v1_19_R1,
+    v1_19_R2;
 
     private final Integer value;
     private final String shortVersion;
