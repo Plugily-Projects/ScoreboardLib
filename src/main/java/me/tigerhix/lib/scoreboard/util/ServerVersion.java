e416cd1a5cc5244897aab6c00750d3a1cfa16c98
Tigerpanzer_02 <37453987+Tigerpanzer02@users.noreply.github.com>
2023-10-10 20:26:18 +0200

    Added 1.20.2

--- src/main/java/me/tigerhix/lib/scoreboard/util/ServerVersion.java
+++ src/main/java/me/tigerhix/lib/scoreboard/util/ServerVersion.java
@@ -35,7 +35,8 @@ public class ServerVersion {
     v1_19_R1,
     v1_19_R2,
     v1_19_R3,
-    v1_20_R1;
+    v1_20_R1,
+    v1_20_R2;
 
     private final Integer value;
     private final String shortVersion;
