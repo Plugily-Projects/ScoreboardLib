1da1c10bdd8e0269aa65ed8d8e98901689b131e5
Lagggpixel <lagggpixel@gmail.com>
2024-06-24 09:53:42 +0100

    feat: added newer version support

--- src/main/java/me/tigerhix/lib/scoreboard/util/ServerVersion.java
+++ src/main/java/me/tigerhix/lib/scoreboard/util/ServerVersion.java
@@ -37,7 +37,11 @@ public class ServerVersion {
     v1_19_R3,
     v1_20_R1,
     v1_20_R2,
-    v1_20_R3;
+    v1_20_R3,
+    v1_20_R4,
+    v1_20_R5,
+    v1_20_R6,
+    v1_21_R1;
 
     private final Integer value;
     private final String shortVersion;
